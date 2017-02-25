af0567b094e6f9792ba09a6829b6d3e2664fb062
Vincenzo Esposito <expovin@gmail.com>
2017-02-25 23:34:36 +0100

    Error handling, router optimization Has been managed the errors in case of QS Server unavailable or for malformed request Now the qrs endpoint just route all the incoming request to the QRS REST Server.

--- routes/qrs.js
+++ routes/qrs.js
@@ -14,132 +14,47 @@ String.prototype.replaceAll = function(search, replacement) {
 };
 
 
-
-
 /* GET users listing. */
-router.get('/:type', function(req, res, next) {
+router.get('/*', function(req, res, next) {
   //res.send('respond with a resource');
      var pathTemplate = '/qrs/[TYPE]?xrfkey=abcdefghijklmnop';
 
      options.hostname = options_template.hostname.replaceAll('[SERVAR-NAME]',req.headers.server_name);
      options.port = options_template.port.replaceAll('[PORT]',req.headers.port);
-     options.path = pathTemplate.replaceAll("[TYPE]",req.params.type);
+     options.path = pathTemplate.replaceAll("[TYPE]",req.params[0]);
      options.method = options_template.method;
      options.headers = options_template.headers;
 
-     options.key = fs.readFileSync(".\\cer\\"+req.headers.server_name+"\\client_key.pem");
-     options.cert = fs.readFileSync(".\\cer\\"+req.headers.server_name+"\\client.pem");
-     options.ca = fs.readFileSync(".\\cer\\"+req.headers.server_name+"\\root.pem");
-
-
-	 console.log(options);     
-     
      var str='';
 
-	 var Inreq = https.get(options, function(Inres) {
-		  // console.log("Got response: " + res.statusCode);
+     try {
+	     options.key = fs.readFileSync("./cer/"+req.headers.server_name+"/client_key.pem");
+	     options.cert = fs.readFileSync("./cer/"+req.headers.server_name+"/client.pem");
+	     options.ca = fs.readFileSync("./cer/"+req.headers.server_name+"/root.pem");  
 
-		    Inres.on('data', function (chunk) {
-	        	console.log("Sono in data");
-	              str += chunk;
-	        });
+		 var Inreq = https.get(options, function(Inres) {
 
-	        Inres.on('end', function () {
-	        	console.log("Sono in end : "+str);
-	            res.json(JSON.parse(str));
-		   
-			}); 
-	    })
+			    Inres.on('data', function (chunk) {
+		              str += chunk;
+		        });
+
+		        Inres.on('end', function () {
+		        	res.status(200);
+		            res.json(JSON.parse(str));
+				}); 
+		    });
+
+			Inreq.on('error', function(error){
+			    res.status(503);
+			    res.json(error);
+			});
+
+
+     } catch (err) {
+     	res.status(401);
+     	res.json(err);
+     } 
 })
-
-.get('/:type/:id', function(req, reqs, next){
-
-	var pathTemplate = '/qrs/[TYPE]/[ID]?xrfkey=abcdefghijklmnop';
-    console.log(pathTemplate);
-
-     var endPoint = pathTemplate.replaceAll("[TYPE]",req.params.type);
-     var endPoint = endPoint.replaceAll("[ID]",req.params.id);
-     console.log(endPoint);
-     options.path=endPoint;
-     
-     var str='';
-
-	 var req = https.get(options, function(res) {
-		  // console.log("Got response: " + res.statusCode);
-
-		    res.on('data', function (chunk) {
-	        	console.log("Sono in data");
-	              str += chunk;
-	        });
-
-	        res.on('end', function () {
-	        	console.log("Sono in end : "+str);
-	            reqs.json(JSON.parse(str));
-		   
-			}); 
-	    })
-})
-
-.get('/:type/:id/:command', function(req, reqs, next){
-
-	var pathTemplate = '/qrs/[TYPE]/[ID]/[COMMAND]?xrfkey=abcdefghijklmnop';
-    console.log(pathTemplate);
-
-     var endPoint = pathTemplate.replaceAll("[TYPE]",req.params.type);
-     var endPoint = endPoint.replaceAll("[ID]",req.params.id);
-     var endPoint = endPoint.replaceAll("[COMMAND]",req.params.command);
-     console.log(endPoint);
-     options.path=endPoint;
-     
-     var str='';
-
-	 var req = https.get(options, function(res) {
-		  // console.log("Got response: " + res.statusCode);
-
-		    res.on('data', function (chunk) {
-	        	console.log("Sono in data");
-	              str += chunk;
-	        });
-
-	        res.on('end', function () {
-	        	console.log("Sono in end : "+str);
-	            reqs.json(JSON.parse(str));
-		   
-			}); 
-	    })
-})
-
-.get('/:prefix/:type/:id/:command/:options', function(req, reqs, next){
-
-	var pathTemplate = '/qrs/[PREFIX]/[TYPE]/[ID]/[COMMAND]/[OPTIONS]?xrfkey=abcdefghijklmnop';
-    console.log(pathTemplate);
-
-     var endPoint = pathTemplate.replaceAll("[TYPE]",req.params.type);
-     var endPoint = endPoint.replaceAll("[ID]",req.params.id);
-     var endPoint = endPoint.replaceAll("[COMMAND]",req.params.command);
-     var endPoint = endPoint.replaceAll("[PREFIX]",req.params.prefix);
-     var endPoint = endPoint.replaceAll("[OPTIONS]",req.params.options);
-     console.log(endPoint);
-     options.path=endPoint;
-     
-     var str='';
-
-	 var req = https.get(options, function(res) {
-		  // console.log("Got response: " + res.statusCode);
-
-		    res.on('data', function (chunk) {
-	        	console.log("Sono in data");
-	              str += chunk;
-	        });
-
-	        res.on('end', function () {
-	        	console.log("Sono in end : "+str);
-	            reqs.json(JSON.parse(str));
-		   
-			}); 
-	    })
-})
-
 
 ;
 
